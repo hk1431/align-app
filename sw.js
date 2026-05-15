3a967ace1559163e1f5c217b2cff45ebf9685d37
hk1431 <sharmaa.deepak82@gmail.com>
2026-05-15 11:36:14 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'align-v1';
+const ASSETS = [
+  '/align-app/align.html',
+  '/align-app/index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() =>
+      caches.match(e.request)
+    )
+  );
+});
